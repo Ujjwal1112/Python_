a8d369ee2a99e4999fa38371fa6a499213438a8c
Ujjwal Shrestha <ujjwalshrestha317@gmail.com>
2023-12-04 08:00:34 +0545

    profile table

--- users/models.py
+++ users/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name="profile")
+    address = models.CharField(max_length=100)
+    contact = models.CharField(max_length=10)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.user.username
+    
+    
+
