b66b9084041b1aa59e2b52b29818acc41b2fa94a
Ujjwal Shrestha <ujjwalshrestha317@gmail.com>
2023-12-11 07:44:11 +0545

    user authentication

--- users/urls.py
+++ users/urls.py
@@ -3,7 +3,8 @@ import users.views as user_views
 
 urlpatterns = [
     path("", user_views.index, name="index"),
-    path("login", user_views.login, name="login"),
-    path("register", user_views.register, name="register"),
-    path("profile", user_views.user_profile, name="profile")
-]
+    path("login", user_views.user_login, name="login"),
+    path("register", user_views.user_register, name="register"),
+    path("profile", user_views.user_profile, name="profile"),
+    path("logout", user_views.user_logout, name="logout")
+]   
--- users/views.py
+++ users/views.py
@@ -1,19 +1,78 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+import users.forms as forms
+from users.models import User, Profile
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
-from django.http import HttpResponse
-
-#Create your views here.
-
 
 def index(request):
     return render(request, "index.html")
 
-def login(request):
+def user_login(request):
+    if request.method == "POST":
+        email = request.POST.get("email")
+        password = request.POST.get("password")
+        print("email: ", email, "password: ", password)
+        check_user = User.objects.filter(email=email)
+        if check_user.exists() is False:
+            error_message = "Email does not exist"
+            messages.error(request, error_message)
+            return redirect("login")
+        username = check_user[0].username
+        valid_user = authenticate(username=username,
+                                  password=password)
+        if valid_user:
+            login(request, valid_user)
+            return redirect("profile")
+        else:
+            error_message = "Invalid email or password"
+            messages.error(request, error_message)
+            return redirect("login")
     return render(request, "login.html")
 
-def register(request):
-    return render(request, "register.html")
+def user_register(request):
+    register_form = forms.RegisterForm()
+    context = {"form": register_form}
+    if request.method == "POST":
+        form_data = request.POST
+        first_name = form_data.get("first_name")
+        last_name = form_data.get("last_name")
+        email = form_data.get("email")
+        password = form_data.get("password")
+        confirm_password = form_data.get("confirm_password")
+        address = form_data.get("address")
+        contact = form_data.get("contact")
+        if password != confirm_password:
+            error_message = "Passwords does not match!"
+            messages.error(request, error_message)
+            return redirect("register")
+        check_user = User.objects.filter(email=email).exists()
+        if check_user is True:
+            error_message = "Email is already taken!"
+            messages.error(request, error_message)
+            return redirect("register")
+        # Create user
+        user = User.objects.create(first_name=first_name, last_name=last_name, 
+                                   email=email, username=email)
+        user.set_password(password)  # use this to hash/encrypt the password
+        user.save()
+        # create profile
+        Profile.objects.create(user=user, address=address, contact=contact)
+        return redirect("login")
+    return render(request, "register.html", context)
 
+
+@login_required()
 def user_profile(request):
-    return render(request, "profile.html")
+    profile = Profile.objects.get(user_id=request.user.pk)
+    context = {"profile": profile}
+    return render(request, "profile.html", context)
+
+
+
+def user_logout(request):
+    logout(request)
+    return redirect("login")
